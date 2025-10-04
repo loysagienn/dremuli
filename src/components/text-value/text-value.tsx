f2eeac6a98c4f9e7cc45f63483da71a54b152fb7
Vladimir Afanasyev <vafanasev@avride.ai>
2025-10-04 18:32:29 +0300

    Better timeline event UI

--- src/components/text-value/text-value.tsx
+++ src/components/text-value/text-value.tsx
@@ -1,16 +1,22 @@
 import React, { useMemo } from "react";
+import styles from "./text-value.module.css";
 
 type TextValueProps = {
   value: string;
 };
 
 export function TextValue({ value }: TextValueProps) {
-  const parts = useMemo(() => value.split("\n"), [value]);
+  const parts = useMemo(
+    () => value.split("\n").map((part) => part.trim()),
+    [value]
+  );
 
   return (
     <>
       {parts.map((part, index) => (
-        <div key={index}>{part}</div>
+        <div key={index} className={styles.part}>
+          {part}
+        </div>
       ))}
     </>
   );
